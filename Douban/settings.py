a1bd37a9c9f5af2293cd0bb3f511211bb02acf5e
guoozz <csguozhen@gmail.com>
2016-02-19 13:08:41 +0800

    context

--- Douban/settings.py
+++ Douban/settings.py
@@ -17,12 +17,12 @@ NEWSPIDER_MODULE = 'Douban.spiders'
 USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36'
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS=4
+CONCURRENT_REQUESTS=1
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY=1.25
+#DOWNLOAD_DELAY=0.2
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN=2
 #CONCURRENT_REQUESTS_PER_IP=16
